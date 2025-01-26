e463ab6ad4bcd525ffb45129deb228f6032e812a
Luneux <yanselpedraza@gmail.com>
2025-01-26 15:56:12 -0600

    Added first item!

--- src/main/java/net/lune/translationmod/TemplateMod.java
+++ src/main/java/net/lune/translationmod/TemplateMod.java
@@ -2,6 +2,7 @@ package net.lune.translationmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lune.translationmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +19,7 @@ public class TemplateMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
